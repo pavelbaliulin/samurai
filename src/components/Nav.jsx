5a4d20007eca3175efe8deb7486adf499b1345c4
unknown <pavel.baliulin@gmail.com>
2021-02-13 11:23:23 +0200

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () =>{
+    return(
+<nav nav className='nav'>
+       <div>
+<div><a>Profile</a></div>
+<div><a>Messages</a></div>
+<div><a>News</a></div>
+<div><a>Music</a></div>
+<div><a>Sattings</a></div>
+
+       </div>
+      </nav>
+    );
+}
+
+export default Nav;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () =>{
+    return(
+<div className='content'>
+     <div>
+      <img src='https://images.wallpaperscraft.ru/image/gory_zakat_ozero_133861_1920x1080.jpg'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div> new post</div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+     </div>
+    );
+}
+
+export default Profile;
